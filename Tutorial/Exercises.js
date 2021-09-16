e303b5b43cf18264ca776612863d000f3bb75809
Sir. ObF <63687784+Plattyz@users.noreply.github.com>
2021-09-15 21:33:43 -0400

    Create Exercises.js

---
+++ Tutorial/Exercises.js
@@ -0,0 +1,47 @@
+//Now, lets practice together!
+//#1
+function RemoveHashtag(input){
+var x = //Try to remove the hashtag from the input!
+console.log(x[0] + " " + x[1])
+}
+
+RemoveHashtag("Hello#World!");
+
+//#2
+function ArrayPractice(){
+var array = ["B", "C", "A"];
+var x = //Here, sort the array and log it to the console!
+}
+
+ArrayPractice();
+
+//#3
+function NaN(input){
+    if(/*Here, check if the input is a number!*/){
+        //If it's not, return to the console that the parameter isn't a number!
+    } else {
+        //Else, print to the console what the number is!
+    }
+}
+
+NaN(//Put your value here to check if your code works!)
+
+//#4
+
+function SquareRoot(input){
+    //Here, we will want to check the square root of a number. I will help you with this one.
+    var x = Math.sqrt(input);
+    //Now, I want you to print the result in the console.
+
+    var y = //Here, I want you to raise the number back to normal (raise it to square).
+}
+
+SquareRoot(2);
+
+//#5
+//The last one, and the hardest.
+//Here, you will make an entire function by your own.
+//This function will take 2 parameters (inputs) and check if they are numbers.
+//If they aren't number, then you will return a console.log() saying that the parameters aren't numbers.
+//Else, you will make a multiplication with those 2 numbers and raise it to square.
+//Then, print on the console the result of this problem. Good luck!
